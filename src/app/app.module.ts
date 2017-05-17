8c5b3a348bde3a1cfd6ef6efba4eca54fc228f6f
Martin Spasov <maspasov@codix.bg>
2017-05-17 19:52:52 +0300

    finish with 22 Binding to Custom Properties.mp4

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { OtherComponent } from './other/other.component';
+import { AnotherComponent } from './other/another.component';
+import { DatabindingComponent } from './databinding/databinding.component';
+import { PropertyBindingComponent } from './databinding/property-binding.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    OtherComponent,
+    AnotherComponent,
+    DatabindingComponent,
+    PropertyBindingComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
