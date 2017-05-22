df197a282c1084e73b76051f891ce34a5410eafd
Martin Spasov <maspasov@codix.bg>
2017-05-22 11:04:17 +0300

    recipe-book

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { OtherComponent } from './other/other.component';
-import { AnotherComponent } from './other/another.component';
-import { DatabindingComponent } from './databinding/databinding.component';
-import { PropertyBindingComponent } from './databinding/property-binding.component';
-import { EventBindingComponent } from './databinding/event-binding.component';
-import { TwoWayBindingComponent } from './databinding/two-way-binding.component';
-import { LifecycleComponent } from './lifecycle.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    OtherComponent,
-    AnotherComponent,
-    DatabindingComponent,
-    PropertyBindingComponent,
-    EventBindingComponent,
-    TwoWayBindingComponent,
-    LifecycleComponent,
-    LifecycleComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -32,5 +17,4 @@ import { LifecycleComponent } from './lifecycle.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
- }
+export class AppModule { }
